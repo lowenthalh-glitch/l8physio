fb5bdb3151be4dbc40925afd17e52ff01fa4ec2a
Sharon Aicler <saichler@gmail.com>
2026-06-19 09:48:33 -0700

    Fix logs

--- go/physio/log-agent/main.go
+++ go/physio/log-agent/main.go
@@ -1,22 +1,47 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
-	"github.com/saichler/l8logfusion/go/agent/logagent"
-	"github.com/saichler/l8types/go/ifs"
-	shared "github.com/saichler/l8utils/go/utils/shared"
+	"github.com/lowenthalh-glitch/l8physio/go/physio/common"
+
+	"github.com/saichler/l8bus/go/overlay/vnic"
+	"github.com/saichler/l8logfusion/go/agent/logs"
+	"github.com/saichler/l8logfusion/go/types/l8logf"
+	"github.com/saichler/l8utils/go/utils/ipsegment"
 )
 
 func main() {
-	nodeIP := os.Getenv("NODE_IP")
-	logPath := os.Getenv("LOGPATH")
-	logFile := os.Getenv("LOGFILE")
-	if logPath == "" {
-		logPath = "/data/logs/physio"
+
+	ip := os.Getenv("NODE_IP")
+	if ip == "" {
+		fmt.Println("Env variable NODE_IP is not set, using machine ip")
+		ip = ipsegment.MachineIP
 	}
 
-	resources := shared.ResourcesOf("logs", 0, 30)
-	resources.Logger().Info("physio log-agent starting, nodeIP=" + nodeIP + ", logPath=" + logPath)
-	logagent.StartAgent(nodeIP, logPath, logFile, resources, ifs.LogLevel_Info)
+	r := common.CreateResources("logagent", true)
+	r.SysConfig().RemoteVnet = ip
+
+	logsDirectory := r.SysConfig().LogConfig.LogDirectory
+
+	logpath := os.Getenv("LOGPATH")
+	if logpath == "" {
+		fmt.Println("Env variable LOGPATH is not set, using " + logsDirectory)
+		logpath = logsDirectory
+	}
+
+	logfile := os.Getenv("LOGFILE")
+	if logfile == "" {
+		fmt.Println("Env variable LOGFILE is not set, using *")
+		logfile = "*"
+	}
+
+	nic := vnic.NewVirtualNetworkInterface(r, nil)
+	nic.Start()
+	nic.WaitForConnection()
+
+	lc := &l8logf.L8LogConfig{Path: logpath, Name: logfile}
+	collector := logs.NewLogCollector(lc, nic)
+	collector.Collect()
 }
